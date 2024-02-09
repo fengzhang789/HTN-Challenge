8bef973d704c6ecdc09e3d3827ae111c1d699132
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-02-09 15:43:16 -0500

    added login feature using Auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-
+import Home from './views/Home'
 
 function App() {
   
   return (
     <>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="events" element={<div>About</div>} />
         <Route path="*" element={<div>404 Not Found :(</div>} />
       </Routes>
---
+++ src/views/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginButton from '../components/LoginButton'
+
+const Home = () => {
+  return (
+    <LoginButton />
+  )
+}
+
+export default Home
